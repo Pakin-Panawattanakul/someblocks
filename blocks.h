9ab67fc92546d35871c4d20b98848f6fcc9f7194
Pakin Panawattanakul <pakin.pan@proton.me>
2026-04-15 02:26:08 +0700

    add colorized status

--- blocks.h
+++ blocks.h
@@ -5,11 +5,10 @@ static const Block blocks[] = {
     //{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
     //30,		0},
 
-    {"", "upower -i \"$(upower -e | grep BAT)\" | awk '/percentage/ {print \"󰁹 \"$2}'", 30, 0},
-    {" ", "wpctl get-volume @DEFAULT_SINK@ | grep 'Volume'  | sed 's/Volume: //'",
-     5, 0},
-    {" ", "date '+%H:%M'", 5, 0},
-    {"󰃭 ", "date '+%b %d (%a)'", 5, 0},
+    {"", "upower -i \"$(upower -e | grep BAT)\" | awk '/percentage/ {print \"<span color=\\\"#fce094\\\">󰁹 \"$2\"</span>\"}'", 60, 0},
+    {"", "wpctl get-volume @DEFAULT_SINK@ | grep 'Volume' | sed 's/Volume: /<span color=\\\"#ffcaff\\\"> /; s/$/<\\\/span>/'", 5, 0},
+    {"", "date '+<span color=\"#b3f6c0\"> %H:%M</span>'", 5, 0},
+    {"", "date '+<span color=\"#a6dbff\">󰃭 %b %d (%a)</span>'", 5, 0},
     /* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
     /* {"", "date '+%b %d (%a) %I:%M%p'",
        0,		10}, */
