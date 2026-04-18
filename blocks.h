e743dcb471d52d78fdcf80f30fa38488eb66296f
Pakin Panawattanakul <pakin.pan@proton.me>
2026-04-18 18:56:22 +0700

    add brightness

--- blocks.h
+++ blocks.h
@@ -4,12 +4,14 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     //{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
     //30,		0},
-    {"", "upower -i \"$(upower -e | grep BAT)\" | awk '/percentage/ {print \"<span color=\\\"#fce094\\\">󰁹 \"$2\"</span>\"}'", 60, 0},
+    {"", "brightnessctl | grep Current | cut -d ' ' -f 4 | sed 's|[()]||g;s|^|<span color=\"#ffc0b9\">󰃟 |;s|$|</span>|'", 0, 3 },
+    {"", "upower -i \"$(upower -e | grep BAT)\" \
+      | awk '/percentage/ {print \"<span color=\\\"#fce094\\\">󰁹 \"$2\"</span>\"}'", 5, 0},
     //{"", "cat /tmp/dwl-keymap",					0,		3},
-    {"", "sed 's/.*/<span color=\"#ffc0b9\">&<\\/span>/' /tmp/dwl-keymap", 0, 3},
-    {"", "wpctl get-volume @DEFAULT_SINK@ | grep 'Volume' | sed 's/Volume: /<span color=\\\"#ffcaff\\\"> /; s/$/<\\\/span>/'", 5, 0},
-    {"", "date '+<span color=\"#b3f6c0\"> %H:%M</span>'", 5, 0},
-    {"", "date '+<span color=\"#a6dbff\">󰃭 %b %d (%a)</span>'", 5, 0},
+    {"", "sed 's/.*/<span color=\"#ffcaff\">&<\\/span>/' /tmp/dwl-keymap", 0, 3},
+    {"", "wpctl get-volume @DEFAULT_SINK@ | grep 'Volume' | sed 's/Volume: /<span color=\\\"#b3f6c0\\\"> /; s/$/<\\\/span>/'", 0, 3},
+    {"", "date '+<span color=\"#8cf8f7\"> %H:%M</span>'", 60, 0},
+    {"", "date '+<span color=\"#a6dbff\">󰃭 %b %d (%a)</span>'", 60, 0},
     /* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
     /* {"", "date '+%b %d (%a) %I:%M%p'",
        0,		10}, */
