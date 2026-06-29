acb0ab8a199378107efcece8099f0a15cd01922a
Pakin Panawattanakul <pakin.pan@proton.me>
2026-06-30 02:22:14 +0700

    change format from pango(somebar) to dwl bar patch format

--- blocks.h
+++ blocks.h
@@ -2,19 +2,13 @@
 // recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    //{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",
-    //30,		0},
-    {"", "brightnessctl i | grep -q backlight && brightnessctl | grep Current | cut -d ' ' -f 4 | sed 's|[()]||g;s|^|<span color=\"#ffc0b9\">󰃟 |;s|$|</span>|'", 0, 3 },
+    {"", "brightnessctl i | grep -q backlight && brightnessctl | grep Current | cut -d ' ' -f 4 | sed 's|[()]||g;s|^|^fg(ffc0b9)󰃟 |;s|$|^fg()|'", 0, 3 },
     {"", "upower -i \"$(upower -e | grep BAT)\" \
-      | awk '/percentage/ {print \"<span color=\\\"#fce094\\\">󰁹 \"$2\"</span>\"}'", 5, 0},
-    //{"", "cat /tmp/dwl-keymap",					0,		3},
-    {"", "sed 's/.*/<span color=\"#ffcaff\">&<\\/span>/' /tmp/dwl-keymap", 0, 3},
-    {"", "wpctl get-volume @DEFAULT_SINK@ | grep 'Volume' | sed 's/Volume: /<span color=\\\"#b3f6c0\\\"> /; s/$/<\\\/span>/'", 0, 3},
-    {"", "date '+<span color=\"#8cf8f7\"> %H:%M</span>'", 60, 0},
-    {"", "date '+<span color=\"#a6dbff\">󰃭 %b %d (%a)</span>'", 60, 0},
-    /* Updates whenever "pkill -SIGRTMIN+10 someblocks" is ran */
-    /* {"", "date '+%b %d (%a) %I:%M%p'",
-       0,		10}, */
+      | awk '/percentage/ {print \"^fg(fce094)󰁹 \"$2\"^fg()\"}'", 5, 0},
+    {"", "sed 's/.*/^fg(ffcaff)&^fg()/' /tmp/dwl-keymap", 0, 3},
+    {"", "wpctl get-volume @DEFAULT_SINK@ | awk '{if(/MUTED/)icon=\"󰝟\";else if($2<0.33)icon=\"󰕿\";else if($2<0.66)icon=\"󰖀\";else icon=\"󰕾\";printf \"^fg(b3f6c0)%s %.0f%%^fg()\",icon,$2*100}'", 1, 3},
+    {"", "date '+^fg(8cf8f7) %H:%M^fg()'", 60, 0},
+    {"", "date '+^fg(a6dbff)󰃭 %b %d (%a)^fg()'", 60, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
